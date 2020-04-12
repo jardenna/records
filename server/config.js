17b8843036c54d11f9bdb14887d3d73cf37574d8
Helle <hbjornum@yahoo.dk>
2020-04-12 11:27:01 +0200

    added custom connection key

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	DB_CONNECTION: 'mongodb+srv://helle:SZkRHrIlhm5Ff4Fo@cluster0-pimzw.mongodb.net/recordproject?retryWrites=true&w=majority',
-	PORT: process.env.PORT || 5000,
+	DB_CONNECTION: 'add connection key here',
+	PORT: process.env.PORT || 'portno',
 	SECRET_KEY: 'create a secret key'
 };
