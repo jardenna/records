aec51b5b9a41b1ba720fecae932df7b4c8aafdec
Helle <hbjornum@yahoo.dk>
2020-05-14 13:48:21 +0200

    added custom hook for search fields

--- src/js/hooks/useFilter.js
+++ src/js/hooks/useFilter.js
@@ -3,10 +3,21 @@ import React from 'react';
 function useFilter(initialState, items) {
    const [values, setValues] = React.useState(initialState);
 
-   const filteredText = items.filter(item => {
-      for (let key in values) {
-         const itemLower = item[key].toLowerCase();
-         if (!itemLower.includes(values[key]) || values[key] !== '')
+   // const filteredText = items.filter(item => {
+   //    for (let key in values) {
+   //       const itemLower = item[key].toLowerCase();
+
+   //       if (!itemLower || !itemLower.includes(values[key]) && values[key] !== '')
+   //          return false;
+   //    }
+   //    return true;
+   // });
+
+
+   const filteredText = items.filter(function (item) {
+      for (var key in values) {
+
+         if (!item[key].toLowerCase().includes(values[key].toLowerCase()) && values[key] !== '')
             return false;
       }
       return true;
