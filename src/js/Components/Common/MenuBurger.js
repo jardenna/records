c384df8d11acced76552e8a4472f96c7552f9047
Helle <hbjornum@yahoo.dk>
2020-07-24 17:27:52 +0200

    styled burger menu

--- src/js/Components/Common/MenuBurger.js
+++ src/js/Components/Common/MenuBurger.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function MenuBurger({ onClick }) {
    return (
-      <span
-         className="mobile-button"
-         onClick={onClick}
-      />
+      <div className="menu-btn-wrapper" onClick={onClick}>
+         <span className="menu-btn" >
+            <span className="menu-btn-burger" />
+         </span>
+      </div>
    );
 }
 
 export default MenuBurger;
+
+MenuBurger.propTypes = {
+   onClick: PropTypes.func.isRequired
+};
