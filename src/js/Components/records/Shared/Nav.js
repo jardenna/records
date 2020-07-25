912d395119bd2e8bbd9dbba50f7d42a2e2915f41
Helle <hbjornum@yahoo.dk>
2020-07-25 16:20:22 +0200

    added activeclass to main menu

--- src/js/Components/records/Shared/Nav.js
+++ src/js/Components/records/Shared/Nav.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { routes } from '@common/constants/nav';
 import MenuBurger from '@commonReact/MenuBurger';
 import { Switch, Route } from 'react-router-dom';
 import useToggle from '@hooks/useToggle';
 import useScroll from '@hooks/useScroll';
-
 
 function Nav() {
    const ref = React.useRef(null);
@@ -13,11 +12,13 @@ function Nav() {
    const [toggle, selected] = useToggle([]);
    const [isSticky] = useScroll(ref);
 
+   const history = useHistory();
+   const pathname = history.location.pathname;
+
    const onToggleNav = (value) => {
       toggle(value);
 
    };
-
 
    return (
       <div className={`${!selected.includes('mobile') ? '' : 'open-nav'} ${isSticky ? 'sticky' : ''} main-nav`} ref={ref}>
@@ -31,7 +32,7 @@ function Nav() {
             <section className="main-wrapper">
                <ul>
                   {routes.map(route => (
-                     route.link ? <li className="main-nav-items" key={route.path} id={route.path} onClick={() => onToggleNav('mobile')}>
+                     route.link ? <li className={`${pathname === route.path ? 'active' : ''} main-nav-items`} key={route.path} id={route.path} onClick={() => onToggleNav('mobile', route.path)}>
                         <Link to={route.path}>{route.link}</Link>
                      </li> : null
                   ))}
@@ -40,7 +41,6 @@ function Nav() {
             <div className={`main-wrapper main-headline ${isSticky ? 'sticky' : ''} `} ref={ref}>
                <Switch>
                   {routes.map(route => {
-
                      return (
                         <Route key={route.path} exact={route.exact} path={route.path} component={route.sideBar} />
                      );
